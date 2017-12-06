618e831cd965105a93f77cf49fcf53c2650492b0
MasterMedo <mislav.vuletic@gmail.com>
2017-12-06 17:18:28 +0100

    Sorting files to folders

--- AoC2017/Day2.py
+++
@@ -1,9 +0,0 @@
-def digits(string):
-    return [int(n) for n in string.split()]
-
-
-with open("./Day2_input.txt") as f:
-  rows = [digits(line) for line in f.read().strip().splitlines()]
-
-print(sum(max(row)-min(row) for row in rows))
-print(sum(a/b for row in rows for a in row for b in row if (a % b == 0 and a != b)))
