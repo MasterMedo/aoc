fe7598945a7e12e99463865212c6aa50b4aa5b89
MasterMedo <mislav.vuletic@gmail.com>
2017-12-10 11:40:22 +0100

    Better solution to Day9 2017 in python

---
+++ AoC2017/src/Day9B.py
@@ -0,0 +1,16 @@
+with open("../inputs/Day9_input.txt") as f:
+  data = list(f.read().replace('\n',''))
+
+i = lvl = depth = garbage = 0
+ExFlag = GaFlag = False
+while i < len(data):
+  if(data[i] == '{' and not GaFlag): depth += 1
+  elif(data[i] == '}' and not GaFlag): lvl += depth; depth -= 1
+  elif(data[i] == '<' and not GaFlag): GaFlag = True
+  elif(data[i] == '>' and GaFlag): GaFlag = False
+  elif(data[i] == '!' and GaFlag): i += 1
+  elif(GaFlag): garbage += 1
+  i += 1
+
+print lvl
+print garbage
