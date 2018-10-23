4114abc69cd79a8b6565ccb09dee9d9e52077b21
MasterMedo <mislav.vuletic@gmail.com>
2018-10-24 01:37:05 +0200

    Solution to day 04 2015 in python

--- 2015/day/04.py
+++ 2015/day/04.py
@@ -1,2 +1,12 @@
-with open("../inputs/04.txt") as f:
-    input = int(f.read())
+import hashlib
+import itertools
+
+with open("../input/04.txt") as f:
+    input = f.read().strip()
+
+for i in itertools.count():
+    hex=hashlib.md5((input+str(i)).encode()).hexdigest()
+    # for first part only 5 zeros '00000'
+    if hex.startswith('000000'):
+        print(i)
+        break
