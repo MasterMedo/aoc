369f8867ae04970fe3e002eae459c6888c5f220c
MasterMedo <mislav.vuletic@gmail.com>
2018-10-29 23:36:05 +0100

    Solution to day 03 2015 in python

---
+++ 2015/day/03.py
@@ -0,0 +1,20 @@
+f = {\
+        "^": lambda xy: (xy[0] - 1, xy[1]),
+        "v": lambda xy: (xy[0] + 1, xy[1]),
+        "<": lambda xy: (xy[0], xy[1] - 1),
+        ">": lambda xy: (xy[0], xy[1] + 1)
+    }
+
+with open('../input/03.txt') as fp:
+    data = fp.read().strip()
+
+grid, xy = {(0, 0): 1}, [[0, 0], [0, 0]]
+for i in range(len(data)):
+    # mod = 0
+    mod = i % 2
+    xy[mod] = f[data[i]](xy[mod])
+    if xy[mod] not in grid:
+        grid[xy[mod]] = 0
+    grid[xy[mod]] += 1
+
+print len(grid)
