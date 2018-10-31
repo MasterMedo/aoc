1c2f4cedcaec1fcaac8943914c11b3e99cb87474
MasterMedo <mislav.vuletic@gmail.com>
2018-11-01 00:15:23 +0100

    improvement: 08 2015

--- 2015/day/08.py
+++ 2015/day/08.py
@@ -1,11 +1,5 @@
 with open('../input/08.txt') as fp:
     data = fp.read().strip().splitlines()
 
-l, d, r = 0, 0, 0
-for i in data:
-    l += len(i)
-    d += len(i.decode('unicode_escape')) - 2
-    r += len(i.encode('unicode_escape')) + 2 + i.count('\"')
-
-print l - d
-print r - l
+print sum([len(i) - len(i.decode('unicode_escape')) + 2 for i in data])
+print sum([len(i.encode('unicode_escape')) + 2 + i.count('\"') - len(i) for i in data])
