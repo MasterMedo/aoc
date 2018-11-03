51b2743b917a0aa48a058dba745132be2ce796e6
MasterMedo <mislav.vuletic@gmail.com>
2018-11-03 14:12:41 +0100

    improvement: 03 2017

--- 2017/day/03.py
+++ 2017/day/03.py
@@ -1,34 +1,26 @@
-# Part 1
-# Sadly, it only works on inputs on the left side of the spiral
-input = 325489
-a = 1
-b = 0
-c = 3
-while a < input:
-  a = c*c
+# part2 initially done by hand, here is one neat solution inspired by /u/akho_
+
+def nex(x, y):
+    if x == y == 0:        return 1, 0
+    if x > -y and x > y:   return x, y + 1
+    if x > -y and x <= y:  return x - 1, y
+    if x <= -y and x < y:  return x, y - 1
+    if x <= -y and x >= y: return x + 1, y
+
+with open('../input/03.txt') as fp:
+    data = int(fp.read().strip())
+
+a, b, c = 1, 0, 3
+while a < data:
+  a = c * c
   b += 1
   c += 2
 c -= 2
-print "Solution to part1:", (c-1)/2 - (input - (a-c+1)) + b
+print (c - 1) / 2 - (data - (a - c + 1)) + b
 
-# Part 2
-# Not my solution (a really bad one as well - this was for no whitespace golfing)
-input = 325489
-a = [-1,0,1]
-r = (1,) * 99
-x = y = s = n = 0
-d = [[0 for g in r] for h in r]
-d[0][0] = u = 1
-while 1:
-  if s == 0:
-    n += 1
-    s = 2 * n
-    u =- u
-  s -= 1
-  o = s//n * u
-  x += o
-  y += u - o
-  i = d[x][y] = sum([d[x+j][y+k] for j in a for k in a])
-  if i > input:
-    print "Solution to part 2:", i
-    break
+x = y = 0
+m = {(0, 0): 1}
+while m[(x, y)] <= data:
+    x, y = nex(x, y)
+    m[(x, y)] = sum(m.get((x + i, y + j), 0) for i in [-1, 0, 1] for j in [0, 1, -1])
+print m[(x, y)]
