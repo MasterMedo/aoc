a567d073cec49a1042f2122e9c83bde3dbbce20b
MasterMedo <mislav.vuletic@gmail.com>
2018-11-03 15:32:18 +0100

    improvement: 06 2017

--- 2017/day/06.py
+++ 2017/day/06.py
@@ -1,29 +1,17 @@
-def digits(string):
-    return [int(n) for n in string.split()]
-
-def solution(block):
-  i = 0
-  blocks = []
-  while (sum(1 for b in blocks if b == block)!=1):
-    z = []
-    for o in block:
-      z.append(o)
-    blocks.append(z)
-    i += 1
-    j = 0
-    tmp = max(block)
-    while block[j] != tmp:
-      j += 1
-    block[j] = 0
-    for k in range(tmp):
-      j += 1
-      if j > len(block)-1:
-        j %= len(block)
-      block[j] += 1
-  print i
-  return block
+def solve(block):
+    count, seen = 0, set()
+    while not seen.intersection({tuple(block)}):
+        count += 1
+        seen.add(tuple(block))
+        top = max(block)
+        i = block.index(top)
+        block[i] = 0
+        for j in range(top):
+            block[(i + j + 1) % len(block)] += 1
+    print count
+    return block
 
 with open("../input/06.txt") as f:
-  rows = [digits(line) for line in f.read().strip().splitlines()]
+    block = map(int, f.read().strip().split())
 
-solution(solution(rows[0]))
+solve(solve(block))
