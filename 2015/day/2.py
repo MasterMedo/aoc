11f9810909262a58b4fb66cb53a7f20c9a2fd757
MasterMedo <mislav.vuletic@gmail.com>
2018-11-04 23:42:29 +0100

    improvement: 02 2015

--- 2015/day/2.py
+++ 2015/day/2.py
@@ -2,8 +2,12 @@ from operator import mul
 from itertools import combinations as comb
 
 with open("../input/2.txt") as f:
-  rows = [map(int, line.split('x')) for line in f.readlines()]
+  edges = [map(int, line.split('x')) for line in f.readlines()]
 
-print sum(2 * side for row in rows for side in map(lambda x: x[0] * x[1], comb(row, 2))) \
-        + sum(min(map(lambda x: x[0] * x[1], comb(row, 2))) for row in rows)
-print sum(2 * sum(sorted(row)[:2]) + reduce(mul, row) for row in rows)
+s = 0
+for edge in edges:
+    areas = map(lambda x: x[0] * x[1], comb(edge, 2))
+    s += sum(map((2).__mul__, areas)) + min(areas)
+print s
+
+print sum(2 * sum(sorted(edge)[:2]) + reduce(mul, edge) for edge in edges)
