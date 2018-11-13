984cd6731ecffbce85d1f6e586d42fb2cb8bc146
MasterMedo <mislav.vuletic@gmail.com>
2018-11-13 21:39:12 +0100

    improvement 4 2016

--- 2016/day/4.py
+++ 2016/day/4.py
@@ -1,11 +1,7 @@
-from collections import Counter
-from itertools import groupby
-from operator import itemgetter
-
 with open('../input/4.txt') as f:
     data = [(''.join(line.split('-')[:-1]), line[:-2].split('-')[-1].split('[')) for line in f.readlines()]
 
-print sum(int(i[1][0]) for i in data if set(i[1][1]) == set(''.join(''.join(sorted(*zip(*j[1])[:1])) for j in groupby(Counter(i[0]).most_common(), key=itemgetter(1)))[:5]))
+print sum(int(i[1][0]) for i in data if set(i[1][1]) == set(list(set(sorted(sorted(i[0]), key=i[0].count, reverse=True)))[:5]))
 
 for line in data:
     if 'north' in ''.join(chr((ord(i) - 97 + int(line[1][0])) % 26 + 97) for i in line[0]):
