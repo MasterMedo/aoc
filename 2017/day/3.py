d56c461c9e974615a065e5dc6b6dab06aa6f4256
MasterMedo <mislav.vuletic@gmail.com>
2018-11-19 16:07:46 +0100

    improvement 3 2017

--- 2017/day/3.py
+++ 2017/day/3.py
@@ -1,22 +1,18 @@
 # part2 initially done by hand, here is one neat solution inspired by /u/akho_
+from math import sqrt
 
 def nex(x, y):
-    if x == y == 0:        return 1, 0
-    if x > -y and x > y:   return x, y + 1
-    if x > -y and x <= y:  return x - 1, y
-    if x <= -y and x < y:  return x, y - 1
+    if x ==  y == 0:       return 1, 0
+    if x >  -y and x >  y: return x, y + 1
+    if x >  -y and x <= y: return x - 1, y
+    if x <= -y and x <  y: return x, y - 1
     if x <= -y and x >= y: return x + 1, y
 
 with open('../input/3.txt') as f:
     data = int(f.read().strip())
 
-a, b, c = 1, 0, 3
-while a < data:
-  a = c * c
-  b += 1
-  c += 2
-c -= 2
-print (c - 1) / 2 - (data - (a - c + 1)) + b
+x = int(sqrt(data)) + 1
+print (x - 1) / 2 - (data - (x * x - x + 1)) + x / 2
 
 x = y = 0
 m = {(0, 0): 1}
