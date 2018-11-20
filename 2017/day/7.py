a80eca492ba82e3b3aaa19f03916eeae2775e0b2
MasterMedo <mislav.vuletic@gmail.com>
2018-11-20 15:05:03 +0100

    improvement 7 2017

--- 2017/day/7.py
+++ 2017/day/7.py
@@ -4,18 +4,17 @@ f = open('../input/7.txt').readlines()
 kids = dict((i.split()[0], i[:-1].replace('-> ', '').replace(',', '').split()[2:]) for i in f)
 weight = dict((i.split()[0], int(i.split()[1].replace(')', '').replace('(', ''))) for i in f)
 
-root = set(kids).difference(set(chain(*kids.values()))).pop()
+print set(kids).difference(set(chain(*kids.values()))).pop()
 
-total = {root: -float('inf')}
-while total[root] == -float('inf'):
+total = dict((kid, -float('inf')) for kid in kids)
+while sum(total.values()) == -float('inf'):
     parent = kids.keys()[0]
     children = kids.pop(parent)
-    above = [total.get(child, -float('inf')) for child in children]
+    above = [total[child] for child in children]
     total[parent] = weight[parent] + sum(above)
     if total[parent] == -float('inf'):
         kids[parent] = children
     elif len(above) and above.count(above[0]) != len(above):
-        node = [x for x in children if above.count(total[x]) == 1][0]
-        print root
+        node = [x for x in children if above.count(total[x]) == 1].pop()
         print weight[node] + sum(set(above)) - 2 * total[node]
         break
