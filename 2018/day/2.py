d9773ec90d4f6e22f58df0b493a0af81635f6c65
MasterMedo <mislav.vuletic@gmail.com>
2018-12-06 02:01:08 +0100

    improvement 2 2018

--- 2018/day/2.py
+++ 2018/day/2.py
@@ -4,5 +4,5 @@ from itertools import chain
 
 data = open('../input/2.txt').read().strip().splitlines()
 
-print reduce(mul, Counter(chain(*[set(Counter(k).values()).intersection(set([2, 3])) for k in data])).values())
+print reduce(mul, Counter(chain(*[set(Counter(i).values()).intersection(set([2, 3])) for i in data])).values())
 print next(key for i in range(len(data[0])) for key, val in Counter(k[:i] + k[i+1:] for k in data).iteritems() if val == 2)
