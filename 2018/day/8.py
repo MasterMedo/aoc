98606692bd5648a0d05c657a72b7312e84f9d5a7
MasterMedo <mislav.vuletic@gmail.com>
2018-12-08 06:35:47 +0100

    solution 8 2018

---
+++ 2018/day/8.py
@@ -0,0 +1,18 @@
+def solve(child, meta):
+    if child == 0:
+        return sum(next(data) for _ in range(meta))
+    value = 0
+    children = {}
+    for i in range(1, child+1):
+        #value += solve(next(data), next(data))
+        children[i] = solve(next(data), next(data))
+    for _ in range(meta):
+        #value += next(data)
+        value += children.get(next(data), 0)
+    return value
+
+data = iter(map(int, open('../input/8.txt').read().split()))
+
+print solve(next(data), next(data))
+
+
