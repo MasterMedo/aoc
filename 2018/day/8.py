a93972826eca6a3a1f1a563df8c895d2e5c8def9
MasterMedo <mislav.vuletic@gmail.com>
2018-12-08 21:36:01 +0100

    improvement 8 2018

--- 2018/day/8.py
+++ 2018/day/8.py
@@ -1,15 +1,17 @@
 def solve(child, meta):
     if child == 0:
-        return sum(next(data) for _ in range(meta))
-    value = 0
+        return [sum(next(data) for _ in range(meta))]*2
     children = {}
+    total = value = 0
     for i in range(1, child+1):
-        #value += solve(next(data), next(data))
-        children[i] = solve(next(data), next(data))
+        _total, _value = solve(next(data), next(data))
+        total += _total
+        children[i] = _value
     for _ in range(meta):
-        #value += next(data)
-        value += children.get(next(data), 0)
-    return value
+        _next = next(data)
+        total += _next
+        value += children.get(_next, 0)
+    return total, value
 
 data = iter(map(int, open('../input/8.txt').read().split()))
 
