86341d0f631ed904b9caab47154ec57b314b01d6
MasterMedo <mislav.vuletic@gmail.com>
2018-12-10 23:52:36 +0100

    improvement 10 2018

--- 2018/day/10.py
+++ 2018/day/10.py
@@ -4,20 +4,15 @@ from itertools import count
 data = [map(int, findall(r'-?\d+', i)) for i in open('../input/10.txt').readlines()]
 
 area = lambda x1, x2, y1, y2: (x2 - x1)*(y2 - y1)
-coord = lambda (x, y, vx, vy): (x + vx*sec, y + vy*sec)
 boundaries = lambda xs, ys: [min(xs), max(xs), min(ys), max(ys)]
+coord = lambda data, sec: [(x + vx*sec, y + vy*sec) for x, y, vx, vy in data]
+size = lambda data, sec: area(*boundaries(*zip(*coord(data, sec))))
 
-min_box = float('inf')
-for sec in count():
-    box = area(*boundaries(*zip(*map(coord, data))))
-    if box > min_box:
-        break
-    min_box = box
+sec = next(sec for sec in count() if size(data, sec) < size(data, sec+1))
 
-sec -= 1
-data = map(coord, data)
+data = coord(data, sec)
 x1, x2, y1, y2 = boundaries(*zip(*data))
 for j in range(y1, y2 + 1):
     print ''.join('#' if (i, j) in data else ' ' for i in range(x1, x2 + 1))
 
-print(sec)
+print sec
