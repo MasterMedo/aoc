438cea8d442523d735b5e8761c7f3715b4d9bcc0
MasterMedo <mislav.vuletic@gmail.com>
2018-12-11 00:01:11 +0100

    improvement 10 2018

--- 2018/day/10.py
+++ 2018/day/10.py
@@ -3,16 +3,16 @@ from itertools import count
 
 data = [map(int, findall(r'-?\d+', i)) for i in open('../input/10.txt').readlines()]
 
-area = lambda x1, x2, y1, y2: (x2 - x1)*(y2 - y1)
-boundaries = lambda xs, ys: [min(xs), max(xs), min(ys), max(ys)]
-coord = lambda data, sec: [(x + vx*sec, y + vy*sec) for x, y, vx, vy in data]
-size = lambda data, sec: area(*boundaries(*zip(*coord(data, sec))))
+area       = lambda x1, x2, y1, y2: (x2 - x1)*(y2 - y1)
+coords     = lambda data, sec:      [(x + vx*sec, y + vy*sec) for x, y, vx, vy in data]
+boundaries = lambda xs, ys:         [min(xs), max(xs), min(ys), max(ys)]
+box_size   = lambda data, sec:      area(*boundaries(*zip(*coords(data, sec))))
 
-sec = next(sec for sec in count() if size(data, sec) < size(data, sec+1))
+sec = next(sec for sec in count() if box_size(data, sec) < box_size(data, sec + 1))
 
-data = coord(data, sec)
-x1, x2, y1, y2 = boundaries(*zip(*data))
+points = coords(data, sec)
+x1, x2, y1, y2 = boundaries(*zip(*points))
 for j in range(y1, y2 + 1):
-    print ''.join('#' if (i, j) in data else ' ' for i in range(x1, x2 + 1))
+    print ''.join('#' if (i, j) in points else ' ' for i in range(x1, x2 + 1))
 
 print sec
