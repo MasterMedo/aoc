2704984a8d983677e3b136cca9c0baadc1529a7d
MasterMedo <mislav.vuletic@gmail.com>
2018-12-13 17:39:23 +0100

    improvement 5 2018

--- 2018/day/5.py
+++ 2018/day/5.py
@@ -1,4 +1,4 @@
-purge = lambda s: reduce(lambda x, y: x[:-1] if x and abs(ord(x[-1])-ord(y)) == 32 else x+y, s)
+purge = lambda s: reduce(lambda x, y: x[:-1] if x and ord(x[-1])^32 == ord(y) else x+y, s)
 data = purge(open('../input/5.txt').read().strip())
 
 print len(data)
