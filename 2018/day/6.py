2572e3351e5ebd10d81f37c52bf8bb86b11b0519
MasterMedo <mislav.vuletic@gmail.com>
2018-12-14 22:29:38 +0100

    improvement 6 2018

--- 2018/day/6.py
+++ 2018/day/6.py
@@ -1,18 +1,13 @@
-data = [map(int, i.split(', ')) for i in open('../input/6.txt').readlines()]
-
-side = max(max(zip(*data)[0]), max(zip(*data)[1]))
 squares = lambda: ((x, y) for x in xrange(side) for y in xrange(side))
 distance = lambda x, y: [abs(x-i) + abs(y-j) for i, j in data]
 closest = lambda x, y: next(n for n, (i, j) in enumerate(data) if not 'm' in list(locals().keys()) and locals().update({'m': min(distance(x, y))}) or abs(x-i) + abs(y-j) == locals()['m'])
 
+data = [map(int, i.split(', ')) for i in open('../input/6.txt').readlines()]
+
+side = max(max(zip(*data)[0]), max(zip(*data)[1]))
 eqidistant = set((x, y) for x, y in squares() if distance(x, y).count(min(distance(x, y))) > 1)
 grid = {(x, y): closest(x, y) if (x, y) not in eqidistant else -1 for x, y in squares()}
 inf = set(grid[x, y] for edge in xrange(side) for x, y in [(edge, side-1), (edge, 0), (side-1, edge), (0, edge)])
 
 print max(grid.values().count(n) for n in xrange(len(data)) if n not in inf)
 print sum(sum(distance(x, y)) < 10000 for x, y in squares())
-
-# explanation of the 'closest' variable in the code, TIL about "locals()" in in-place list comprehension
-# def closest(x, y):
-#     m = min(distance(x, y))
-#     return next(n for n, (i, j) in enumerate(data) if abs(x-i) + abs(y-j) == min(distance(x, y)))
