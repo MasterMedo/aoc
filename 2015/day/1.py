f86dbf77caab82117c14df86579b0c38516b57d4
MasterMedo <mislav.vuletic@gmail.com>
2018-12-26 07:09:13 +0100

    improvement 1 2015

--- 2015/day/1.py
+++ 2015/day/1.py
@@ -1,5 +1,5 @@
 with open('../input/1.txt') as f:
-    data = [1 if i == "(" else -1 for i in f.read().strip()]
+    data = [1 if i == '(' else -1 for i in f.read().strip()]
 
 print sum(data)
-print min([i for i in xrange(len(data)) if sum(data[:i]) == -1])
+print next(i for i in range(len(data)) if sum(data[:i]) == -1)
