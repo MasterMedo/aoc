623245c47a1adad2924a39d27b6ce536f736aa5e
MasterMedo <mislav.vuletic@gmail.com>
2018-12-27 22:39:06 +0100

    improvement 10 2016

--- 2016/day/10.py
+++ 2016/day/10.py
@@ -1,34 +1,25 @@
-from operator import mul
+from collections import defaultdict
 
-bots = {}
+bot = defaultdict(list)
+output = defaultdict(list)
+
 with open('../input/10.txt') as f:
-    for i in (j.split() for j in sorted(f.readlines())):
-        if i[0] == 'bot':
-            bots['bot' + i[1]] = {'low': i[5] + i[6], 'high': i[-2] + i[-1], 'vals': []}
-            if i[5] == 'output':
-                bots[i[5] + i[6]] = {'vals': []}
-            if i[-2] == 'output':
-                bots[i[-2] + i[-1]] = {'vals': []}
+    children = {}
+    for line in map(str.split, f):
+        if line[0] == 'value':
+            bot[int(line[-1])].append(int(line[1]))
         else:
-            bots['bot' + i[-1]]['vals'].append(int(i[1]))
-            if len(bots['bot' + i[-1]]['vals']) == 2:
-                two_val_bots = {'bot' + i[-1]: bots['bot' + i[-1]]}
+            children[int(line[1])] = line[ 5] +'['+ line[ 6] +']', \
+                                     line[-2] +'['+ line[-1] +']'
 
-while len(two_val_bots):
-    for bot in two_val_bots.keys():
-        if len(bots[bots[bot]['high']]['vals']) == 2 \
-        or len(bots[bots[bot]['high']]['vals']) == 2:
-            continue
+while bot:
+    for bot_n, v in dict(bot).iteritems():
+        if len(v) == 2:
+            v1, v2 = sorted(bot.pop(bot_n))
+            lower, higher = children[bot_n]
+            eval(lower).append(v1)
+            eval(higher).append(v2)
+            if v1 == 17 and v2 == 61:
+                print(bot_n)
 
-        cur = two_val_bots.pop(bot)
-        cur['vals'] = sorted(cur['vals'])
-
-        if set(cur['vals']) == {17, 61}:
-            print bot
-
-        for val in ['high', 'low']:
-            bots[cur[val]]['vals'].append(cur['vals'].pop())
-            if len(bots[cur[val]]['vals']) == 2:
-                two_val_bots[cur[val]] = bots[cur[val]]
-        
-print reduce(mul, [bots['output0']['vals'].pop(), bots['output1']['vals'].pop(), bots['output2']['vals'].pop()])
+print output[0][0] * output[1][0] * output[2][0]
