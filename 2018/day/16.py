13ee129c668136d00f8ba064d1086707d81d68f9
MasterMedo <mislav.vuletic@gmail.com>
2019-01-01 22:29:04 +0100

    improvement 16 2018

--- 2018/day/16.py
+++ 2018/day/16.py
@@ -1,10 +1,6 @@
 from re import findall
 
-to_ints = lambda x: (map(int, findall('-?\d+', i)) for i in x.split('\n') if i)
-with open('../input/16.txt') as f:
-    test, program = map(to_ints, f.read().split('\n\n\n\n'))
-
-commands = {
+ops = {
         'addr': lambda r, A, B: r[A] + r[B],
         'addi': lambda r, A, B: r[A] + B,
         'mulr': lambda r, A, B: r[A] * r[B],
@@ -13,8 +9,8 @@ commands = {
         'bani': lambda r, A, B: r[A] & B,
         'borr': lambda r, A, B: r[A] | r[B],
         'bori': lambda r, A, B: r[A] | B,
-        'setr': lambda r, A, B: r[A],
-        'seti': lambda r, A, B: A,
+        'setr': lambda r, A, _: r[A],
+        'seti': lambda r, A, _: A,
         'gtir': lambda r, A, B: int(A > r[B]),
         'gtri': lambda r, A, B: int(r[A] > B),
         'gtrr': lambda r, A, B: int(r[A] > r[B]),
@@ -23,24 +19,22 @@ commands = {
         'eqrr': lambda r, A, B: int(r[A] == r[B]),
         }
 
-ambiguities = 0
-translate = {}
-for before, (number, A, B, C), after in zip(*[test]*3):
-    potential = set()
-    for opcode in commands:
-        if commands[opcode](before, A, B) == after[C]:
-            potential.add(opcode)
+to_ints = lambda x: (map(int, findall('-?\d+', i)) for i in x.split('\n') if i)
+with open('../input/16.txt') as f:
+    test, program = map(to_ints, f.read().split('\n\n\n\n'))
 
-    if len(potential) >= 3:
-        ambiguities += 1
-        
-    potential = potential.difference(set(translate.values()))
-    if len(potential) == 1:
-        translate[number] = potential.pop()
+count, trans = 0, {}
+for before, (n, A, B, C), after in zip(*[test]*3):
+    cand = set(code for code in ops if ops[code](before, A, B) == after[C])
+    if len(cand) >= 3:
+        count += 1
+    cand = cand - set(trans.values())
+    if len(cand) == 1:
+        trans[n] = cand.pop()
 
-r = [0, 0, 0, 0]
-for number, A, B, C in program:
-    r[C] = commands[translate[number]](r, A, B)
+r = [0] * 4
+for n, A, B, C in program:
+    r[C] = ops[trans[n]](r, A, B)
 
-print ambiguities
+print count
 print r[0]
