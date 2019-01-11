fe8cd2f63e89af673fd44d2fbdaeeb20ded65c65
MasterMedo <mislav.vuletic@gmail.com>
2019-01-11 03:23:18 +0100

    improvement 13 2018

--- 2018/day/13.py
+++ 2018/day/13.py
@@ -1,33 +1,30 @@
 from itertools import count
 
-def move(xy, way, turn):
-    c = track[xy]
-    if c == '+':
-        way = (way-1)%4 if turn == 0 else way if turn == 1 else (way+1)%4
-        turn = (turn+1)%3
-    if c in '/\\':
-        way = 3-way if c == '/' else 3*way-3
-    return xy + 1j**way, way, turn
-
 track, cars, orientation = {}, {}, '>v<^'
 with open('../input/13.txt') as f:
-    for y, l in enumerate(f.read().splitlines()):
-        for x, c in enumerate(l):
+    for y, row in enumerate(f.read().splitlines()):
+        for x, c in enumerate(row):
             track[x + y*1j] = c
             if c in orientation:
                 track[x + y*1j] = '-' if c in '<>' else '|'
                 cars[x + y*1j] = (orientation.index(c), 0)
 
 for tick in count():
-    for xy in sorted(cars, key = lambda ij: (ij.imag, ij.real)):
+    for xy in sorted(cars, key = lambda i: (i.imag, i.real)):
         if xy not in cars:
             continue
-        xy, way, turn = move(xy, *cars.pop(xy))
+        c, (w, t) = track[xy], cars.pop(xy) # w=way, t=turn
+        if c == '+':
+            w = (t-1)**2*(w+t-1)%4 + (2*t-t**2)*w
+            t = (t+1)%3
+        if c in '/\\':
+            w = 3-w if c == '/' else 3*w-3
+        xy += 1j**w
         if xy in cars:
             cars.pop(xy)
-            print 'crash '+str(xy)
+            print 'crash ' + str(xy)
             continue
-        cars.update({xy: (way, turn)})
+        cars.update({xy: (w, t)})
     if len(cars) == 1:
-        print 'winner '+str(set(cars).pop())
+        print 'winner ' + str(set(cars).pop())
         break
