3ddbdea28de601afdba52d16adbb45f75c80b783
MasterMedo <mislav.vuletic@gmail.com>
2020-04-03 12:14:14 +0200

    improvement 2 2019

--- 2019/day/2.py
+++ 2019/day/2.py
@@ -1,18 +1,14 @@
-from operator import add, mul
-
 with open('../input/2.txt') as f:
     data = list(map(int, f.read().split(',')))
 
-def run(data, a, b):
-    i, l = 0, data[:]
-    l[1], l[2] = a, b
-    op = lambda f: f(l[l[i+1]],l[l[i+2]])
-    while True:
+def run(l, i, j):
+    l[1], l[2] = i, j
+    for i in range(0, len(l), 4):
         if l[i] == 99:
             return l[0]
-        l[l[i+3]] = op(add if l[i] == 1 else mul)
-        i += 4
+        a, b = l[l[i+1]], l[l[i+2]]
+        l[l[i+3]] = a + b if l[i] == 1 else a * b
 
-print(run(data, 12, 2))
-print(next(100*a+b for a in range(100)
-    for b in range(100) if run(data, a, b) == 19690720))
+print(run(data[:], 12, 2))
+print(next(100*i+j for i in range(100)
+    for j in range(100) if run(data[:], i, j) == 19690720))
