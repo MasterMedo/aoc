b72ffafe412c2cebcfd9224c9507c43123c77a0d
MasterMedo <mislav.vuletic@gmail.com>
2020-04-05 18:04:27 +0200

    improvement 17 2018

--- 2018/day/17.py
+++ 2018/day/17.py
@@ -1,45 +1,45 @@
+import re
+
 clay, water, stillwater = set(), set(), set()
 with open('../input/17.txt') as f:
-    for l, r in map(str.split, f):
-        a, b = map(int, r[2:].split('..'))
-        for w in range(a, b + 1):
-            if l[0] == 'x':
-                clay.add((int(l[2:-1]), w))
-            else:
-                clay.add((w, int(l[2:-1])))
+    for line in f:
+        x, a, b = map(int, re.findall(r'\d+', line))
+        for y in range(a, b+1):
+            clay.add((x, y) if line[0] == 'x' else (y, x))
 
 xmin, ymin = map(min, zip(*clay))
 xmax, ymax = map(max, zip(*clay))
 
 down, leftright = {(500, 0)}, set()
-while down | leftright:
+while down or leftright:
     while down:
         x, y = down.pop()
-        if y > ymax:
-            break
-        water.add((x, y))
-        if (x, y+1) in clay | stillwater:
-            leftright.add((x, y))
-        elif (x, y+1) not in water - stillwater:
-            down.add((x, y+1))
+        while y <= ymax and (x, y) not in water:
+            water.add((x,y))
+            if (x, y+1) in clay or (x, y+1) in stillwater:
+                leftright.add((x, y))
+                break
+            y += 1
+
     while leftright:
         x, y = leftright.pop()
-        stream, spills = {(x, y)}, set()
+        water.add((x, y))
+        spills = False
+        stream = {(x, y)}
         for way in [-1, 1]:
             i, j = x+way, y
             while (i, j) not in clay:
                 stream.add((i, j))
-                if (i, j+1) not in clay | stillwater:
-                    spills.add((i, j))
+                if (i, j+1) not in clay and (i, j+1) not in stillwater:
+                    down.add((i, j))
+                    spills = True
                     break
+                water.add((i, j))
                 i += way
-        if spills:
-            water.update(stream)
-            down.update(spills)
+
         if not spills:
-            water.update(stream)
             stillwater.update(stream)
             leftright.add((x, y-1))
 
-print len(water) - ymin
-print len(stillwater)
+print(len(water) - ymin)
+print(len(stillwater))
