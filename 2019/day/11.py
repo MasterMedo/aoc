b030e24ab9b267f1cc224121a6eb9f3c87a54d9c
MasterMedo <mislav.vuletic@gmail.com>
2020-04-05 21:28:06 +0200

    improvement 11 2019

--- 2019/day/11.py
+++ 2019/day/11.py
@@ -3,25 +3,24 @@ from collections import defaultdict
 with open('../input/11.txt') as f:
     l = defaultdict(int, dict(enumerate(map(int, f.read().split(',')))))
 
-i = 0
-relative_base = 0
-xy, robot_instructions, direction = 0j, 0, 1j
+i = r = z = 0
+xy, direction = 0j, 1j
 neighbour = {(1j,0):-1, (1j,1):1, (1,0):1j, (1,1):-1j,
              (-1j,0):1, (-1j,1):-1, (-1,0):-1j, (-1,1):1j}
 
-grid = defaultdict(int, {0: 1}) # part 1: 1 -> 0
+grid = defaultdict(int, {0: 1}) # comment for part 1
+# grid = defaultdict(int)         # uncomment for part 1
 while True:
     optcode = l[i] % 100
     mode = list(reversed(list(map(int, str(l[i]//100)))))
+    arg = lambda x: l[state(x)]
     def state(x):
         if x-i-1 >= len(mode) or mode[x-i-1] == 0:
             return l[x]
         elif mode[x-i-1] == 1:
             return x
         elif mode[x-i-1] == 2:
-            return relative_base + l[x]
-
-    arg = lambda x: l[state(x)]
+            return r + l[x]
 
     if   optcode == 99: # halt
         break
@@ -39,13 +38,12 @@ while True:
         i += 2
 
     elif optcode ==  4: # output
-        if robot_instructions % 2 == 0:
+        if z % 2 == 0:
             grid[xy] = arg(i+1)
         else:
             direction = neighbour[direction, arg(i+1)]
             xy += direction
-
-        robot_instructions += 1
+        z += 1
         i += 2
 
     elif optcode ==  5: # jump if true
@@ -68,16 +66,17 @@ while True:
         l[state(i+3)] = 1 if arg(i+1) == arg(i+2) else 0
         i += 4
 
-    elif optcode == 9: # relative_base
-        relative_base += arg(i+1)
+    elif optcode ==  9: # relative_base
+        r += arg(i+1)
         i += 2
 
-    else: # error
+    else:               # error
         print(f'error: {optcode}')
         exit()
 
+
 print(len(grid))
-for y in range(-100, 100):
-    for x in range(-100, 100):
+for y in range(0, -6, -1):
+    for x in range(0, 40):
         print('#' if grid[x+y*1j] else ' ', end='')
     print()
