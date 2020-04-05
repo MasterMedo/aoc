45957d2eb9e4faf41d06f1ba969a83111e73c0ee
MasterMedo <mislav.vuletic@gmail.com>
2020-04-05 21:29:48 +0200

    improvement 9 2019

--- 2019/day/9.py
+++ 2019/day/9.py
@@ -3,20 +3,18 @@ from collections import defaultdict
 with open('../input/9.txt') as f:
     l = defaultdict(int, dict(enumerate(map(int, f.read().split(',')))))
 
-i = 0
-relative_base = 0
+i = r = 0
 while True:
     optcode = l[i] % 100
     mode = list(reversed(list(map(int, str(l[i]//100)))))
+    arg = lambda x: l[state(x)]
     def state(x):
         if x-i-1 >= len(mode) or mode[x-i-1] == 0:
             return l[x]
         elif mode[x-i-1] == 1:
             return x
         elif mode[x-i-1] == 2:
-            return relative_base + l[x]
-
-    arg = lambda x: l[state(x)]
+            return r + l[x]
 
     if   optcode == 99: # halt
         break
@@ -57,10 +55,10 @@ while True:
         l[state(i+3)] = 1 if arg(i+1) == arg(i+2) else 0
         i += 4
 
-    elif optcode == 9: # relative_base
-        relative_base += arg(i+1)
+    elif optcode ==  9: # relative_base
+        r += arg(i+1)
         i += 2
 
-    else: # error
+    else:               # error
         print(f'error: {optcode}')
         exit()
