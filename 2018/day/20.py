8183751957493f81702c7cc524d64feb30613723
MasterMedo <mislav.vuletic@gmail.com>
2020-04-06 23:52:54 +0200

    improvement 20 2018

--- 2018/day/20.py
+++ 2018/day/20.py
@@ -1,15 +1,13 @@
-from collections import defaultdict, deque
-
 with open('../input/20.txt') as f:
     path = f.read()[1:-2]
 
 way = {'N': 1j, 'S': -1j, 'E': 1, 'W': -1}
-depth, stack = defaultdict(int), deque()
+depth, stack = {0: 0}, []
 xy = zw = 0
 for c in path:
     if c in 'NEWS':
         xy += way[c]
-        if depth[xy] == 0 or depth[xy] > depth[zw]+1:
+        if xy not in depth or depth[xy] > depth[zw]+1:
             depth[xy] = depth[zw]+1
     elif c == '(':
         stack.append(xy)
