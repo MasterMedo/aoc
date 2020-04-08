d480a0bd455598ecf1564523160e6e8ed8c00549
MasterMedo <mislav.vuletic@gmail.com>
2020-04-08 23:53:50 +0200

    improvement 2 2015

--- 2015/day/2.py
+++ 2015/day/2.py
@@ -1,13 +1,5 @@
-from operator import mul
-from itertools import combinations as comb
-
 with open("../input/2.txt") as f:
-  edges = [map(int, line.split('x')) for line in f.readlines()]
+  edges = [list(map(int, line.split('x'))) for line in f.readlines()]
 
-s = 0
-for edge in edges:
-    areas = map(lambda x: x[0] * x[1], comb(edge, 2))
-    s += sum(map((2).__mul__, areas)) + min(areas)
-print s
-
-print sum(2 * sum(sorted(edge)[:2]) + reduce(mul, edge) for edge in edges)
+print(sum(2 * (x*y + y*z + z*x) + min(x*y, y*z, z*x) for x, y, z in edges))
+print(sum(2 * sum(sorted([x, y, z])[:2]) + x*y*z for x, y, z in edges))
