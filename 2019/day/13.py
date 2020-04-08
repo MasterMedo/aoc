7b875c2915306b47e8e76db699db81e2b815070c
MasterMedo <mislav.vuletic@gmail.com>
2020-04-08 23:00:54 +0200

    improvement 13 2019

--- 2019/day/13.py
+++ 2019/day/13.py
@@ -1,36 +1,38 @@
 from collections import defaultdict
-from blessed import Terminal
 intcode = __import__('9').intcode
+from blessed import Terminal
+term = Terminal()
 
 with open('../input/13.txt') as f:
-    l = defaultdict(int, dict(enumerate(map(int, f.read().split(',')))))
-
-term = Terminal()
-block = {0: ' ', 1: '|', 2: '#', 3: '_', 4: '*'}
-key = defaultdict(int, {'h': -1, 'LEFT': -1, 'l': 1, 'RIGHT': 1})
+    tape = defaultdict(int, dict(enumerate(map(int, f.read().split(',')))))
 
 def player_input():
+    key = {'h': -1, 'LEFT': -1, 'l': 1, 'RIGHT': 1}
     while True:
-        print(term.home, end='', flush=True)
-        for y in range(50, 0, -1):
-            print(''.join(block[grid[x, y]] for x in range(50)), flush=True)
+        # print(term.home, end='', flush=True)
+        # for y in range(50, 0, -1):
+        #     print(''.join(' |#_*'[grid.get((x, y), 0)] for x in range(50)))
+        # char = term.inkey()
+        # yield key.get(char.name[4:] if char.is_sequence else char, 0)
+        yield -1 if ball < player else ball > player
 
-        char = term.inkey()
-        yield key[char] if not char.is_sequence else key[char.name[4:]]
+def handle_output():
+    global x, y, player, ball, score
+    while True:
+        x = yield
+        y = yield
+        t = yield
+        if   t <= 4: grid[x, y] = t
+        if   t == 3: player = x
+        elif t == 4: ball = x
+        elif t >  4: score = t
 
-score = x = y = 0
-grid = defaultdict(int)
-l[0] = 2 # comment for part one
+score, grid = 0, {}
+tape[0] = 2 # comment for part one
+(output := handle_output()).send(None)
 with term.cbreak(), term.hidden_cursor():
-    for i, o in enumerate(intcode(l, player_input())):
-        if i % 3 == 0:
-            x = o
-        elif i % 3 == 1:
-            y = o
-        elif o > 4:
-            score = o
-        else:
-            grid[x, y] = o
+    for instruction in intcode(tape, player_input()):
+        output.send(instruction)
 
 print(f'blocks: {sum(grid[k] == 2 for k in grid)}')
 print(f'score: {score}')
