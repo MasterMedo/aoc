f5aef199d8b87733cb8f790d7563290f7378de45
MasterMedo <mislav.vuletic@gmail.com>
2020-04-08 20:30:54 +0200

    improvement 9 2019

--- 2019/day/9.py
+++ 2019/day/9.py
@@ -1,49 +1,28 @@
 from collections import defaultdict
 
-def intcode(l, inp):
-    i = r = 0
-    jump = [None, 4, 4, 2, 2, 0, 0, 4, 4, 2]
+def intcode(tape, int_input):
+    i = rb = 0
+    jump  = [4, 4, 2, 2, 0, 0, 4, 4, 2]
+    modes = lambda x, m: [tape[x+i], x+i, rb+tape[x+i]][m]
     while True:
-        opcode, mode = l[i] % 100, str(l[i]//100).zfill(3)[::-1]
-        arg = lambda x: l[pos(x)]
-        def pos(x):
-            d = {'0': l[x+i], '1': x+i, '2': r + l[x+i]}
-            return d[mode[x-1]]
+        opcode = tape[i] % 100
+        mode = str(tape[i]//100).zfill(3)[::-1]
+        arg = lambda x: tape[pos(x)]
+        pos = lambda x: modes(x, int(mode[x-1]))
 
-        if   opcode == 99: # halt
-            break
+        if   opcode == 99: break
+        elif opcode ==  1: tape[pos(3)] = arg(1) + arg(2)
+        elif opcode ==  2: tape[pos(3)] = arg(1) * arg(2)
+        elif opcode ==  3: tape[pos(1)] = next(int_input)
+        elif opcode ==  4: yield arg(1)
+        elif opcode ==  5: i = arg(2) if arg(1) else i+3
+        elif opcode ==  6: i = arg(2) if not arg(1) else i+3
+        elif opcode ==  7: tape[pos(3)] = arg(1) < arg(2)
+        elif opcode ==  8: tape[pos(3)] = arg(1) == arg(2)
+        elif opcode ==  9: rb += arg(1)
+        else: raise Exception(f'invalid opcode {opcode}')
 
-        elif opcode ==  1: # add
-            l[pos(3)] = arg(1) + arg(2)
-
-        elif opcode ==  2: # multiply
-            l[pos(3)] = arg(1) * arg(2)
-
-        elif opcode ==  3: # input
-            l[pos(1)] = next(inp)
-
-        elif opcode ==  4: # output
-            yield arg(1)
-
-        elif opcode ==  5: # jump if true
-            i = arg(2) if arg(1) else i+3
-
-        elif opcode ==  6: # jump if false
-            i = arg(2) if not arg(1) else i+3
-
-        elif opcode ==  7: # less than
-            l[pos(3)] = 1 if arg(1) < arg(2) else 0
-
-        elif opcode ==  8: # equals
-            l[pos(3)] = 1 if arg(1) == arg(2) else 0
-
-        elif opcode ==  9: # relative_base
-            r += arg(1)
-
-        else:              # error
-            raise Exception(f'invalid opcode {opcode}')
-
-        i += jump[opcode]
+        i += jump[opcode-1]
 
 if __name__ == '__main__':
     with open('../input/9.txt') as f:
