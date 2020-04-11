b52d7557bf89ff03b7bb7f438a4a878a084f73b3
MasterMedo <mislav.vuletic@gmail.com>
2020-04-11 14:54:25 +0200

    improvement 18 2017

--- 2017/day/18.py
+++ 2017/day/18.py
@@ -1,36 +1,35 @@
-def num(a, regs):
-    try:
-        return int(a)
-    except ValueError:
-        if a not in regs:
-            regs[a] = 0
-        return regs[a]
+from time import sleep
+from collections import deque
 
-def run(a):
-    regs = {}
-    j = 0
+def run(instructions, p=0):
+    i, reg = 0, {'p': p}
     while True:
-        i = data[j]
-        if i[0] == 'snd':
-             snd = num(i[1], regs)
-        elif i[0] == 'set':
-            regs[i[1]] = num(i[2], regs)
-        elif i[0] == 'add':
-            regs[i[1]] = num(i[1], regs) + num(i[2], regs)
-        elif i[0] == 'mul':
-            regs[i[1]] = num(i[1], regs) * num(i[2], regs)
-        elif i[0] == 'mod':
-            regs[i[1]] = num(i[1], regs) % num(i[2], regs)
-        elif i[0] == 'rcv':
-            if num(i[1], regs) > 0:
-                print snd
-                break
-        elif i[0] == 'jgz':
-            if num(i[1], regs) > 0:
-                j = j + num(i[2], regs) - 1
-        j += 1
+        code, *args = instructions[i]
+        num = lambda x: int(x) if x[-1].isdigit() else reg.get(x, 0)
+        arg = lambda x: num(args[x-1])
+        if   code == 'snd': queue[p].append(sound := arg(1))
+        elif code == 'set': reg[args[0]] = arg(2)
+        elif code == 'add': reg[args[0]] = arg(1) + arg(2)
+        elif code == 'mul': reg[args[0]] = arg(1) * arg(2)
+        elif code == 'mod': reg[args[0]] = arg(1) % arg(2)
+        elif code == 'rcv': reg[args[0]] = yield
+        elif code == 'jgz': i += arg(2)-1 if arg(1) > 0 else 0
+        i += 1
 
 with open("../input/18.txt") as f:
-    data = [x[:-1].split() for x in f.readlines()]
+    data = list(map(str.split, f))
 
-run()
+counter = 0
+queue = [deque([]), deque([])]
+next(program0 := run(data, 0))
+next(program1 := run(data, 1))
+print(queue[0][-1])
+
+while queue[0] or queue[1]:
+    if queue[0]:
+        program1.send(queue[0].popleft())
+    if queue[1]:
+        counter += 1
+        program0.send(queue[1].popleft())
+
+print(counter)
