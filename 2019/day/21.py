13c633fd318f2ad4ed367c18319801077bfcd06a
MasterMedo <mislav.vuletic@gmail.com>
2020-04-20 15:23:00 +0200

    solution 21 2019

---
+++ 2019/day/21.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+intcode = __import__('9').intcode
+
+with open('../input/21.txt') as f:
+    tape = dict(enumerate(map(int, f.read().split(','))))
+
+part1 = """
+OR A J
+AND B J
+AND C J
+NOT J J
+AND D J
+WALK
+""" # ~(ABC)D
+
+part2 = """
+OR A J
+AND B J
+AND C J
+NOT J J
+OR E T
+OR H T
+AND T J
+AND D J
+RUN
+"""
+springscript = (ord(c) for c in part2[1:])
+for output in intcode(defaultdict(int, tape), springscript):
+    if output < 1000:
+        print(chr(output), end='')
+
+print(output)
