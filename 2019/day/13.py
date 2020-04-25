88dd5a86b739b3fe8b6d9b7aeb4be5d3b29b736d
MasterMedo <mislav.vuletic@gmail.com>
2020-04-25 11:25:21 +0200

    improvement 13 2019

--- 2019/day/13.py
+++ 2019/day/13.py
@@ -12,15 +12,15 @@ with open('../input/13.txt') as f:
 def player_input():
     key = {'h': -1, 'LEFT': -1, 'l': 1, 'RIGHT': 1}
     while True:
-        sleep(0.1)
-        print(term.home, end='', flush=True)
-        for y in range(50, 0, -1):
-            print(''.join(' |#_*'[grid.get((x, y), 0)] for x in range(50)))
+        # sleep(0.1)
+        # print(term.home, end='', flush=True)
+        # for y in range(50, 0, -1):
+        #     print(''.join(' |#_*'[grid.get((x, y), 0)] for x in range(50)))
         # char = term.inkey()
         # yield key.get(char.name[4:] if char.is_sequence else char, 0)
         yield -1 if ball < player else ball > player
 
-score, grid = 0, {}
+grid = {}
 tape[0] = 2 # comment for part one
 game = intcode(tape, player_input())
 with term.cbreak(), term.hidden_cursor():
@@ -30,7 +30,6 @@ with term.cbreak(), term.hidden_cursor():
         if   t <= 4: grid[x, y] = t
         if   t == 3: player = x
         elif t == 4: ball = x
-        elif t >  4: score = t
 
 print(f'blocks: {sum(grid[i] == 2 for i in grid)}')
-print(f'score: {score}')
+print(f'score: {t}')
