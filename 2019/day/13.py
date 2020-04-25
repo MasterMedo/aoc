31614a8c2085d10ac894b2f47e48e8217f37c5ef
MasterMedo <mislav.vuletic@gmail.com>
2020-04-25 09:42:53 +0200

    improvement 13 2019

--- 2019/day/13.py
+++ 2019/day/13.py
@@ -16,23 +16,17 @@ def player_input():
         # yield key.get(char.name[4:] if char.is_sequence else char, 0)
         yield -1 if ball < player else ball > player
 
-def handle_output():
-    global x, y, player, ball, score
-    while True:
-        x = yield
-        y = yield
-        t = yield
+score, grid = 0, {}
+tape[0] = 2 # comment for part one
+game = intcode(tape, player_input())
+with term.cbreak(), term.hidden_cursor():
+    for x in game:
+        y = next(game)
+        t = next(game)
         if   t <= 4: grid[x, y] = t
         if   t == 3: player = x
         elif t == 4: ball = x
         elif t >  4: score = t
 
-score, grid = 0, {}
-tape[0] = 2 # comment for part one
-(output := handle_output()).send(None)
-with term.cbreak(), term.hidden_cursor():
-    for instruction in intcode(tape, player_input()):
-        output.send(instruction)
-
-print(f'blocks: {sum(grid[k] == 2 for k in grid)}')
+print(f'blocks: {sum(grid[i] == 2 for i in grid)}')
 print(f'score: {score}')
