3e4cba9bfad1754b1c8332dfe8e451c6092d6dab
MasterMedo <mislav.vuletic@gmail.com>
2020-04-26 13:52:21 +0200

    improvement 8 2019

--- 2019/day/8.py
+++ 2019/day/8.py
@@ -1,4 +1,5 @@
-split_every_n = lambda l, n: [l[i:i+n] for i in range(0, len(l), n)]
+def split_every_n(l, n):
+    return [l[i:i+n] for i in range(0, len(l), n)]
 
 with open('../input/8.txt') as f:
     images = split_every_n(list(map(int, f.read()[:-1])), 25*6)
@@ -8,4 +9,5 @@ print(image.count(1) * image.count(2))
 
 image = ''.join('#' if next(pixel for pixel in layer if pixel != 2) else ' '
                 for layer in zip(*images))
+
 print('\n'.join(split_every_n(image, 25)))
