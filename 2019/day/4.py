dff385d36e3f77867ea7c50a052fda86e659fa06
MasterMedo <mislav.vuletic@gmail.com>
2020-04-26 14:03:58 +0200

    improvement 4 2019

--- 2019/day/4.py
+++ 2019/day/4.py
@@ -1,22 +1,25 @@
 with open('../input/4.txt') as f:
     x, y = map(int, f.read().split('-'))
 
-c = 0
+counter = 0
 for i in range(x, y+1):
-    f = False
-    s = str(i)
-    d = 0
-    for j in range(len(s)-1):
-        if s[j] > s[j+1]:
+    standalone = False
+    password = str(i)
+    digit = 0
+    for j in range(len(password)-1):
+        if password[j] > password[j+1]:
             break
-        if s[j] == s[j+1]:
-            d += 1
-        else:
-            if d == 1:
-                f = True
-            d = 0
-    else:
-        if f or d == 1:
-            c += 1
 
-print(c)
+        if password[j] == password[j+1]:
+            digit += 1
+        else:
+            if digit == 1:
+                standalone = True
+            digit = 0
+
+        # if digit: digit = 1 # uncomment for part1
+    else:
+        if standalone or digit == 1:
+            counter += 1
+
+print(counter)
