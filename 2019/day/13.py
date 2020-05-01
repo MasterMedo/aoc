6d11c8122838c47410cd999ee01b0a0b096b1814
MasterMedo <mislav.vuletic@gmail.com>
2020-05-01 15:26:30 +0200

    improvement 13 2019

--- 2019/day/13.py
+++ 2019/day/13.py
@@ -9,9 +9,7 @@ tape[0] = 2 # comment for part one
 player_input = (-1 if ball < player else ball > player for _ in iter(int, 1))
 game = intcode(tape, player_input)
 
-for x in game:
-    y = next(game)
-    t = next(game)
+for x, y, t in iter(lambda: [next(game) for _ in range(3)], 1):
     if x == -1 and y == 0 and t == 0:
         print(sum(grid[i] == 2 for i in grid))
 
