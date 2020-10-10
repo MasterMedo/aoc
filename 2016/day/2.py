2758c560f0fb66d8d276dd1d6a4f2e9569ad89b3
MasterMedo <mislav.vuletic@gmail.com>
2020-10-10 21:40:16 +0200

    improvement 2 2016

--- 2016/day/2.py
+++ 2016/day/2.py
@@ -1,17 +1,20 @@
-def code(line):
+def walk(instructions):
     x = 13
-    for i in line:
-        if grid.get(x + move[i]):
-            x += move[i]
-    return '%x'.upper() % grid[x]
+    for i in instructions:
+        x_ = x + move[i]
+        if -1 < x_ < len(grid) and grid[x_] != '_':
+            x = x_
 
-with open('../input/2.txt') as f:
-    data = f.read().strip().splitlines()
+    return grid[x]
+
 
 move = {'R': 1, 'L': -1, 'U': -5, 'D': 5}
 
-grid = {7: 1, 8: 2, 9: 3, 12: 4, 13: 5, 14: 6, 17: 7, 18: 8, 19: 9}
-print ''.join(map(code, data))
+with open('../input/2.txt') as f:
+    lines = f.read().strip().splitlines()
 
-grid = {3: 1, 7: 2, 8: 3, 9: 4, 11: 5, 12: 6, 13: 7, 14: 8, 15: 9, 17: 10, 18: 11, 19: 12, 23: 13}
-print ''.join(map(code, data))
+grid = '_____' '_123_' '_456_' '_789_' '_____'
+print(''.join(map(walk, lines)))
+
+grid = '__1__' '_234_' '56789' '_ABC_' '__D__'
+print(''.join(map(walk, lines)))
