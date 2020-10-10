d43754730258136472977cb4f13c1f863627768a
MasterMedo <mislav.vuletic@gmail.com>
2020-10-10 18:09:23 +0200

    improvement 6 2016

--- 2016/day/6.py
+++ 2016/day/6.py
@@ -1,5 +1,5 @@
 with open('../input/6.txt') as f:
-    columns = list(zip(*f.read().split('\n')[:-1]))
+    columns = list(zip(*f.read().splitlines()))
 
-print(''.join(max(set(c), key = c.count) for c in columns))
-print(''.join(min(set(c), key = c.count) for c in columns))
+print(''.join(max(set(c), key=c.count) for c in columns))
+print(''.join(min(set(c), key=c.count) for c in columns))
