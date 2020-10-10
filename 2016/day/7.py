59a244517d5410fd6e2ef7507913e6991ef36e14
MasterMedo <mislav.vuletic@gmail.com>
2020-10-10 20:59:06 +0200

    improvement 7 2016

--- 2016/day/7.py
+++ 2016/day/7.py
@@ -1,7 +1,19 @@
 import re
+
+palindrom = re.compile(r'(\w)(?!\1)(\w)\2\1')
+aba_bab = re.compile(r'(\w)(?!\1)(\w)\1.*@.*\2\1\2')
+
+TLS = SSL = 0
 with open('../input/7.txt') as f:
-    data = [(' '.join(j[::2]), ' '.join(j[1::2])) for j in (i[:-1].replace('[', ']').split(']') for i in f.readlines())]
+    for line in f:
+        segments = re.split(r'\[|\]', line[:-1])
+        outside = ' '.join(segments[::2])
+        inside = ' '.join(segments[1::2])
+        if re.search(palindrom, outside) and not re.search(palindrom, inside):
+            TLS += 1
 
-print sum(1 for i in data if re.search(r'(\w)(?!\1)(\w)\2\1', i[0]) and not re.search(r'(\w)(?!\1)(\w)\2\1', i[1]))
-print sum(1 for i in data if re.search(r'(\w)(?!\1)(\w)\1.*@.*\2\1\2', i[0] + '@' + i[1]))
+        if re.search(aba_bab, inside + '@' + outside):
+            SSL += 1
 
+print(TLS)
+print(SSL)
