c18bbe1072fd8d0fca69841fc0ac5ab9fcfa1748
MasterMedo <mislav.vuletic@gmail.com>
2020-10-10 18:25:26 +0200

    improvement 9 2016

--- 2016/day/9.py
+++ 2016/day/9.py
@@ -3,17 +3,19 @@ def out(string):
     while True:
         if i >= len(string):
             break
+
         if string[i] == '(':
             end = string.index(')', i)
             x, y = map(int, string[i + 1:end].split('x'))
-            # n += x*y - 1
             n += out(string[end + 1:end + 1 + x]) * y - 1
+            # n += x*y - 1
             i = end + x
         i += 1
         n += 1
     return n
 
+
 with open('../input/9.txt') as f:
     data = f.read().strip()
 
-print out(data)
+print(out(data))
