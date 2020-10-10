946fccbf5d5e6024c02ee8066fb7b611d4d82833
MasterMedo <mislav.vuletic@gmail.com>
2020-10-10 18:21:30 +0200

    improvement 8 2016

--- 2016/day/8.py
+++ 2016/day/8.py
@@ -1,19 +1,26 @@
 import numpy as np
+
+
 with open('../input/8.txt') as f:
-    data = [i.split() for i in f.readlines()]
+    lines = [line.split() for line in f]
 
 grid = np.zeros((6, 50))
-for i in data:
-    if i[0] == 'rect':
-        y, x = map(int, i[1].split('x'))
+for words in lines:
+    if words[0] == 'rect':
+        y, x = map(int, words[1].split('x'))
         grid[0: x, 0: y] = np.ones((x, y))
+
     else:
-        x, n = int(i[-3].split('=')[1]), int(i[-1])
-        if i[1] != 'row': grid = np.transpose(grid)
+        x, n = int(words[-3].split('=')[1]), int(words[-1])
+        if words[1] != 'row':
+            grid = np.transpose(grid)
+
         grid[x] = np.roll(grid[x], n)
-        if i[1] != 'row': grid = np.transpose(grid)
 
-print int(np.sum(grid))
+        if words[1] != 'row':
+            grid = np.transpose(grid)
 
-for k in [''.join(map(lambda x: '.' if x == 0 else '#', j)) for j in grid]:
-    print k
+print(int(np.sum(grid)))
+
+for line in grid:
+    print(''.join(' ' if c == 0 else '#' for c in line))
