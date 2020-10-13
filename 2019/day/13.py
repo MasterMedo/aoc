5044e1941b58f5fb6f4a3cda7e77fea69f36358d
MasterMedo <mislav.vuletic@gmail.com>
2020-10-13 13:18:27 +0200

    improvement 13 2019

--- 2019/day/13.py
+++ 2019/day/13.py
@@ -1,11 +1,11 @@
-from collections import defaultdict
 intcode = __import__('9').intcode
 
 with open('../input/13.txt') as f:
     tape = list(map(int, f.read().split(',')))
 
 grid = {}
-tape[0] = 2 # comment for part one
+tape[0] = 2
+ball = player = None
 player_input = (-1 if ball < player else ball > player for _ in iter(int, 1))
 game = intcode(tape, player_input)
 
@@ -13,8 +13,11 @@ for x, y, t in iter(lambda: [next(game) for _ in range(3)], 1):
     if x == -1 and y == 0 and t == 0:
         print(sum(grid[i] == 2 for i in grid))
 
-    if   t <= 4: grid[x, y] = t
-    if   t == 3: player = x
-    elif t == 4: ball = x
+    if t <= 4:
+        grid[x, y] = t
+    if t == 3:
+        player = x
+    elif t == 4:
+        ball = x
 
 print(t)
