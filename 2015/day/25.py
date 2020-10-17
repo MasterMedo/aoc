ee83c88e604d7a94fa847e8f10f811fd5fd397e7
MasterMedo <mislav.vuletic@gmail.com>
2020-10-17 11:10:07 +0200

    improvement 25 2015

--- 2015/day/25.py
+++ 2015/day/25.py
@@ -7,12 +7,10 @@ a, b, c = 20151125, 252533, 33554393
 
 while True:
     t += 1
-    for i, j in zip(xrange(t - 1, 0, -1), xrange(1, t)):
+    for i, j in zip(range(t - 1, 0, -1), range(1, t)):
         num += 1
         if i == x and y == j:
-            for k in xrange(num - 1):
+            for k in range(num - 1):
                 a = a * b % c
-            print a
-            break
-
-
+            print(a)
+            exit()
