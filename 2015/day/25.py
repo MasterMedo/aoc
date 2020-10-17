b77ece4521efd241c31497f619a009704aacf268
MasterMedo <mislav.vuletic@gmail.com>
2020-10-17 11:27:48 +0200

    improvement 25 2015

--- 2015/day/25.py
+++ 2015/day/25.py
@@ -1,16 +1,5 @@
 with open('../input/25.txt') as f:
-    tmp = f.read().strip().replace(',', '').replace('.', '').split()
-    x, y = int(tmp[-3]), int(tmp[-1])
+    row, column = [int(n[:-1]) for n in f.read().split() if n[0].isdigit()]
 
-num, t = 0, 1
-a, b, c = 20151125, 252533, 33554393
-
-while True:
-    t += 1
-    for i, j in zip(range(t - 1, 0, -1), range(1, t)):
-        num += 1
-        if i == x and y == j:
-            for k in range(num - 1):
-                a = a * b % c
-            print(a)
-            exit()
+code = ((row + column)**2 - 3*row-column)//2
+print(pow(252533, code, 33554393)*20151125 % 33554393)
