46dcba4dabeb820bb36c743191a2c4958e8c2af0
MasterMedo <mislav.vuletic@gmail.com>
2020-10-17 09:46:37 +0200

    improvement 6 2015

--- 2015/day/6.py
+++ 2015/day/6.py
@@ -1,18 +1,21 @@
 import numpy as np
 
-f = {
-        "turnon": lambda x, y: x * 1 + y,   # x * 1
-        "turnoff": lambda x, y: x * -1 + y, # x * -1
-        "toggle": lambda x, y: x * 2 + y    # 1 - y
-    }
-
 with open('../input/6.txt') as f:
-    data = [i.replace("turn ", "turn").split() for i in f.readlines()]
+    data = [line.replace(',', ' ').split() for line in f.readlines()]
 
 grid = np.zeros((1000, 1000))
-for line in data:
-    x1, y1, x2, y2 = map(int, ''.join(line[1] + ',' + line[-1]).split(','))
-    grid[x1: x2 + 1, y1: y2 + 1] = f[line[0]](np.ones((x2 - x1 + 1, y2 - y1 + 1)), grid[x1: x2 + 1, y1: y2 + 1])
+for *cmd, x, y, _, z, w in data:
+    x, y, z, w = map(int, [x, y, z, w])
+    if cmd[0] == 'toggle':
+        grid[x:z+1, y:w+1] = 1 - grid[x:z+1, y:w+1]
+        # grid[x:z+1, y:w+1] += 2
+
+    elif cmd[1] == 'on':
+        grid[x:z+1, y:w+1] = 1  # = -> +=
+
+    elif cmd[1] == 'off':
+        grid[x:z+1, y:w+1] = -1  # = -> +=
+
     grid[grid < 0] = 0
 
-print int(np.sum(grid))
+print(int(np.sum(grid)))
