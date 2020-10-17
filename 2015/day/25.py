d456ccd9b86ea784dc75e90468f1c5bc55981eeb
MasterMedo <mislav.vuletic@gmail.com>
2020-10-17 13:07:39 +0200

    improvement 25 2015

--- 2015/day/25.py
+++ 2015/day/25.py
@@ -1,5 +1,5 @@
 with open('../input/25.txt') as f:
     row, column = [int(n[:-1]) for n in f.read().split() if n[0].isdigit()]
 
-code = ((row + column)**2 - 3*row-column)//2
-print(pow(252533, code, 33554393)*20151125 % 33554393)
+code = (column + row - 1) * (column + row) // 2 - row
+print(20151125 * pow(252533, code, 33554393) % 33554393)
