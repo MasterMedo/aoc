6676bf9df397a7f55ad6d3e246bbff7314a709a3
MasterMedo <mislav.vuletic@gmail.com>
2020-10-17 10:51:15 +0200

    improvement 14 2015

--- 2015/day/14.py
+++ 2015/day/14.py
@@ -1,21 +1,21 @@
-import math
 with open('../input/14.txt') as f:
-    raindeers = [[int(w) for w in words if w.isdigit()]
-                    for raindeer, *words in map(str.split, f)]
+    raindeers = [[int(word) for word in line.split() if word.isdigit()]
+                 for line in f]
 
 time = 2503
-print(max((time//(duration+rest)*duration + min(time %(duration+rest),duration)) * speed for speed, duration, rest in raindeers))
+print(max((time // (fly + rest)*fly + min(time % (fly + rest), fly)) * speed
+          for speed, fly, rest in raindeers))
 
 points = [0]*len(raindeers)
 distance = [0]*len(raindeers)
-flying = [duration-1 for _, duration, _ in raindeers]
-for i in range(time):
-    for raindeer, (speed, duration, rest) in enumerate(raindeers):
-        if   flying[raindeer] >= 0:
+flying = [fly-1 for _, fly, _ in raindeers]
+for _ in range(time):
+    for raindeer, (speed, fly, rest) in enumerate(raindeers):
+        if flying[raindeer] >= 0:
             distance[raindeer] += speed
 
         elif flying[raindeer] <= -rest:
-            flying[raindeer] = duration
+            flying[raindeer] = fly
 
         flying[raindeer] -= 1
 
