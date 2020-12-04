33aaf020b4af049d91a23020a10ff2653853b44e
MasterMedo <mislav.vuletic@gmail.com>
2020-12-04 16:15:33 +0100

    improvement 3 2020

--- 2020/day/3.py
+++ 2020/day/3.py
@@ -1,23 +1,16 @@
+from itertools import count
+
 with open('../input/3.txt') as f:
     data = [line.strip() for line in f]
 
-rows = len(data)
-cols = len(data[0])
-forest = []
-for x, y in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
-    col = row = trees = 0
-    while row < rows:
-        row = row % rows
-        col = col % cols
-        if data[row][col] == '#':
+product = 1
+for x, y in [(1, 1), (5, 1), (7, 1), (1, 2), (3, 1)]:
+    trees = 0
+    for row, col in zip(range(0, len(data), y), count(0, x)):
+        if data[row][col % len(data[0])] == '#':
             trees += 1
 
-        col += x
-        row += y
-    forest.append(trees)
+    product *= trees
 
-for t in forest[1:]:
-    forest[0] *= t
-
-print(forest[1])
-print(forest[0])
+print(trees)
+print(product)
