3293a61c7f3619a6da721a36cb90788376dac697
MasterMedo <mislav.vuletic@gmail.com>
2020-12-25 06:34:48 +0100

    solution 25 2020

---
+++ 2020/day/25.py
@@ -0,0 +1,10 @@
+with open('../input/25.txt') as f:
+    me, door = keys = set(map(int, f.read().splitlines()))
+
+mod = 20201227
+loop_size = 1
+subject = 7
+for loop_size in range(10**10):
+    if (public := pow(subject, loop_size, mod)) in keys:
+        print(pow(me if public == door else door, loop_size, mod))
+        break
