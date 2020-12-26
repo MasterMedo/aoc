336c3c8136ffbe293aa98f4eeee7788ddae53c32
MasterMedo <mislav.vuletic@gmail.com>
2020-12-26 10:57:21 +0100

    improvement 23 2020

--- 2020/day/23.py
+++ 2020/day/23.py
@@ -1,22 +1,35 @@
+def emulate(d, label, iterations):
+    for _ in range(iterations):
+        left = d[label]
+        middle = d[left]
+        right = d[middle]
+        dest = (label - 2) % size + 1
+        while dest in (left, middle, right):
+            dest = (dest - 2) % size + 1
+
+        d[label], d[dest], d[right] = d[right], left, d[dest]
+        label = d[label]
+    return d
+
+
 with open('../input/23.txt') as f:
     data = list(map(int, f.read().strip()))
 
+size = len(data)
+d = [1]*(size + 1)
+for i, e in enumerate(data):
+    d[e] = data[(i+2) % size - 1]
+
+d1 = emulate(d[:], data[0], 100)
+
+in_order = [d1[1]]
+for _ in range(size - 2):
+    in_order.append(d1[in_order[-1]])
+
+print(''.join(map(str, in_order)))
+
+d[data[-1]] = size + 1
 size, iterations = 10**6, 10**7
-d = list(range(1, size+2))
-for i, e in enumerate(data[:-1]):
-    d[e] = data[i+1]
-
-d[data[-1]], d[size] = len(data) + 1, data[0]
-label = data[0]
-for _ in range(iterations):
-    left = d[label]
-    middle = d[left]
-    right = d[middle]
-    dest = (label - 2) % size + 1
-    while dest in (left, middle, right):
-        dest = (dest - 2) % size + 1
-
-    d[label], d[dest], d[right] = d[right], left, d[dest]
-    label = d[label]
-
-print(d[1]*d[d[1]])
+d = d + list(range(len(d) + 1, size + 1)) + [data[0]]
+d = emulate(d, data[0], iterations)
+print(d[1] * d[d[1]])
