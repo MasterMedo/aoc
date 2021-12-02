adc3f2b20ed82167e0824f63704222b1f14c57f9
MasterMedo <mislav.vuletic@gmail.com>
2021-12-02 07:52:07 +0100

    improvement 1 2021

--- 2021/day/1.py
+++ 2021/day/1.py
@@ -2,9 +2,4 @@ with open("../input/1.txt") as f:
     depths = list(map(int, f.readlines()))
 
 print(sum(depths[i - 1] < depths[i] for i in range(1, len(depths))))
-print(
-    sum(
-        sum(depths[i - 3 : i]) < sum(depths[i - 2 : i + 1])
-        for i in range(3, len(depths))
-    )
-)
+print(sum(current > last for last, current in zip(depths, depths[3:])))
