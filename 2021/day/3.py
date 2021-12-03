5b4d72b1efaef197b4206d32fa3f22f5528a3a57
MasterMedo <mislav.vuletic@gmail.com>
2021-12-03 07:59:44 +0100

    improvement 3 2021

--- 2021/day/3.py
+++ 2021/day/3.py
@@ -1,35 +1,25 @@
 with open("../input/3.txt") as f:
     data = [list(line.strip()) for line in f]
 
-epsilon = ''
-gamma = ''
-
+epsilon = ""
+gamma = ""
 for bits in zip(*data):
-    zeros = bits.count('0')
+    zeros = bits.count("0")
     ones = len(data) - zeros
-    epsilon += '01'[ones > zeros]
-    gamma += '10'[ones > zeros]
+    epsilon += "01"[ones > zeros]
+    gamma += "10"[ones > zeros]
 
 print(int(epsilon, 2) * int(gamma, 2))
 
-codes = data[:]
-for i in range(len(data[0])):
-    zeros = sum(code[i] == '0' for code in codes)
-    ones = len(codes) - zeros
-    common_bit = '01'[ones >= zeros]
-    codes = [code for code in codes if code[i] == common_bit]
-    if len(codes) == 1:
-        oxygen = ''.join(codes[0])
-        break
 
-codes = data[:]
-for i in range(len(data[0])):
-    zeros = sum(code[i] == '0' for code in codes)
-    ones = len(codes) - zeros
-    common_bit = '01'[ones < zeros]
-    codes = [code for code in codes if code[i] == common_bit]
-    if len(codes) == 1:
-        carbon = ''.join(codes[0])
-        break
+def part_2(codes, common):
+    for i in range(len(data[0])):
+        zeros = sum(code[i] == "0" for code in codes)
+        ones = len(codes) - zeros
+        common_bit = "01"[(ones >= zeros) ^ common]
+        codes = [code for code in codes if code[i] == common_bit]
+        if len(codes) == 1:
+            return int("".join(codes[0]), 2)
 
-print(int(oxygen, 2) * int(carbon, 2))
+
+print(part_2(data[:], True) * part_2(data[:], False))
