65f9b8cea0f4f0c6d3ad4beef445950b3b5cd54c
MasterMedo <mislav.vuletic@gmail.com>
2021-12-04 11:02:21 +0100

    improvement 4 2021

--- 2021/day/4.py
+++ 2021/day/4.py
@@ -1,6 +1,6 @@
 with open("../input/4.txt") as f:
-    numbers = [int(n) for n in next(f).split(",")]
-    next(f)  # discard the first empty line
+    numbers = [int(n) for n in f.readline().split(",")]
+    f.readline()  # discard the first empty line
     boards = []
     board = []
     for line in f:
@@ -13,30 +13,28 @@ with open("../input/4.txt") as f:
 part_1 = None
 part_2 = None
 seen_boards = set()
-for n in numbers:
+for number in numbers:
     for board_index, board in enumerate(boards):
         if board_index in seen_boards:
             continue
 
-        for i in range(5):
-            for j in range(5):
-                if board[i][j][0] == n:
-                    board[i][j][1] = True
+        for i, j in ((i, j) for i in range(5) for j in range(5)):
+            if board[i][j][0] == number:
+                board[i][j][1] = True
+                break
+        else:
+            continue
 
-                    row = all(board[x][j][1] for x in range(5))
-                    col = all(board[i][y][1] for y in range(5))
-                    if row or col:
-                        sum_ = 0
-                        for row in board:
-                            for number, seen in row:
-                                if not seen:
-                                    sum_ += number
+        row = all(board[x][j][1] for x in range(5))
+        col = all(board[i][y][1] for y in range(5))
+        if row or col:
+            sum_ = sum(x for row in board for x, seen in row if not seen)
 
-                        if part_1 is None:
-                            part_1 = sum_ * n
+            if part_1 is None:
+                part_1 = sum_ * number
 
-                        part_2 = sum_ * n
-                        seen_boards.add(board_index)
+            part_2 = sum_ * number
+            seen_boards.add(board_index)
 
 print(part_1)
 print(part_2)
