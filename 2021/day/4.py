cfbfea5abea767f1d87ca2339de6512916e0e4c2
MasterMedo <mislav.vuletic@gmail.com>
2021-12-04 07:16:36 +0100

    improvement 4 2021

--- 2021/day/4.py
+++ 2021/day/4.py
@@ -1,36 +1,42 @@
 with open("../input/4.txt") as f:
-    tickets = list(map(int, next(f).strip().split(",")))
-    next(f)
+    numbers = [int(n) for n in next(f).split(",")]
+    next(f)  # discard the first empty line
     boards = []
-    for board_string in f.read()[:-1].split("\n\n"):
-        board = {}
-        for i, line in enumerate(board_string.split("\n")):
-            for j, n in enumerate(map(int, line.strip().split())):
-                board[i, j] = [n, False]
-
-        boards.append(board)
+    board = []
+    for line in f:
+        if line.strip():
+            board.append([[int(n), False] for n in line.split()])
+        else:
+            boards.append(board)
+            board = []
 
 part_1 = None
 part_2 = None
 seen_boards = set()
-for ticket in tickets:
+for n in numbers:
     for board_index, board in enumerate(boards):
         if board_index in seen_boards:
             continue
 
-        for i, j in board:
-            if board[i, j][0] == ticket:
-                board[i, j][1] = True
+        for i in range(5):
+            for j in range(5):
+                if board[i][j][0] == n:
+                    board[i][j][1] = True
 
-                row = all(board[x, j][1] for x in range(5))
-                col = all(board[i, y][1] for y in range(5))
-                if row or col:
-                    sum_ = sum(n for n, b in board.values() if not b) * ticket
-                    if part_1 is None:
-                        part_1 = sum_
+                    row = all(board[x][j][1] for x in range(5))
+                    col = all(board[i][y][1] for y in range(5))
+                    if row or col:
+                        sum_ = 0
+                        for row in board:
+                            for number, seen in row:
+                                if not seen:
+                                    sum_ += number
 
-                    part_2 = sum_
-                    seen_boards.add(board_index)
+                        if part_1 is None:
+                            part_1 = sum_ * n
+
+                        part_2 = sum_ * n
+                        seen_boards.add(board_index)
 
 print(part_1)
 print(part_2)
