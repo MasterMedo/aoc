6204b9b542788ba755565e1d3747ed94ea47bccd
MasterMedo <mislav.vuletic@gmail.com>
2021-12-06 06:55:29 +0100

    improvement 6 2021

--- 2021/day/6.py
+++ 2021/day/6.py
@@ -1,16 +1,14 @@
-from collections import defaultdict
-
-d = defaultdict(int)
+lanternfish = [0] * 9
 with open("../input/6.txt") as f:
     for n in f.read().split(","):
-        d[int(n)] += 1
+        lanternfish[int(n)] += 1
 
 for days_passed in range(256):
-    d2 = {i: d[i + 1] for i in range(8)}
-    d2[6] += d[0]
-    d2[8] = d[0]
-    d = d2
+    fish = [lanternfish[i + 1] for i in range(8)] + [0]
+    fish[6] += lanternfish[0]
+    fish[8] += lanternfish[0]
+    lanternfish = fish
     if days_passed == 79:
-        print(sum(d.values()))
+        print(sum(lanternfish))
 
-print(sum(d.values()))
+print(sum(lanternfish))
