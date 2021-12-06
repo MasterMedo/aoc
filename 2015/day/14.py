a5b64cf4d6410b24db42aa89b5863eee6dbab46b
MasterMedo <mislav.vuletic@gmail.com>
2021-12-06 14:40:45 +0100

    improvement 14 2015

--- 2015/day/14.py
+++ 2015/day/14.py
@@ -1,27 +1,27 @@
 with open('../input/14.txt') as f:
-    raindeers = [[int(word) for word in line.split() if word.isdigit()]
+    reindeers = [[int(word) for word in line.split() if word.isdigit()]
                  for line in f]
 
 time = 2503
 print(max((time // (fly + rest)*fly + min(time % (fly + rest), fly)) * speed
-          for speed, fly, rest in raindeers))
+          for speed, fly, rest in reindeers))
 
-points = [0]*len(raindeers)
-distance = [0]*len(raindeers)
-flying = [fly-1 for _, fly, _ in raindeers]
+points = [0]*len(reindeers)
+distance = [0]*len(reindeers)
+flying = [fly-1 for _, fly, _ in reindeers]
 for _ in range(time):
-    for raindeer, (speed, fly, rest) in enumerate(raindeers):
-        if flying[raindeer] >= 0:
-            distance[raindeer] += speed
+    for reindeer, (speed, fly, rest) in enumerate(reindeers):
+        if flying[reindeer] >= 0:
+            distance[reindeer] += speed
 
-        elif flying[raindeer] <= -rest:
-            flying[raindeer] = fly
+        elif flying[reindeer] <= -rest:
+            flying[reindeer] = fly
 
-        flying[raindeer] -= 1
+        flying[reindeer] -= 1
 
     max_distance = max(distance)
-    for raindeer in range(len(distance)):
-        if distance[raindeer] == max_distance:
-            points[raindeer] += 1
+    for reindeer in range(len(distance)):
+        if distance[reindeer] == max_distance:
+            points[reindeer] += 1
 
 print(max(points))
