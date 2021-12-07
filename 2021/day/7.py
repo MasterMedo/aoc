32154b072ca22a16d09a839031aa76b272dc753d
MasterMedo <mislav.vuletic@gmail.com>
2021-12-07 06:28:11 +0100

    improvement 7 2021

--- 2021/day/7.py
+++ 2021/day/7.py
@@ -1,6 +1,6 @@
-import math
 from math import factorial as f
 from functools import cache
+
 
 @cache
 def second_factorial(n: int) -> int:
@@ -9,13 +9,14 @@ def second_factorial(n: int) -> int:
     elif n == 1:
         return 1
     else:
-        return f(n + 1) // f(2) // f(n -1)
+        return f(n + 1) // f(2) // f(n - 1)
 
-with open('../input/7.txt') as fp:
-    data = [int(n) for n in fp.read().split(',')]
 
-part_1 = float('inf')
-part_2 = float('inf')
+with open("../input/7.txt") as fp:
+    data = [int(n) for n in fp.read().split(",")]
+
+part_1 = float("inf")
+part_2 = float("inf")
 for i in range(max(data)):
     part_1 = min(part_1, sum(abs(i - n) for n in data))
     part_2 = min(part_2, sum(second_factorial(abs(i - n)) for n in data))
