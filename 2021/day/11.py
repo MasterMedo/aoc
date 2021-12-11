359667de9cb1c8d787d4622528b56d31e1a89b7a
MasterMedo <mislav.vuletic@gmail.com>
2021-12-11 19:47:43 +0100

    improvement 11 2021

--- 2021/day/11.py
+++ 2021/day/11.py
@@ -1,42 +1,38 @@
-from itertools import count
-
 data = {}
 with open("../input/11.txt") as f:
     for y, row in enumerate(f.readlines()):
         for x, n in enumerate(row.strip()):
             data[(x, y)] = int(n)
 
-height = y + 1
-width = x + 1
 border = ((1, -1), (1, 0), (1, 1), (0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1))
-flashes = 0
-for step in count(1):
-    stack = []
-    seen = set()
+step = 0
+total_flashes = 0
+while True:
+    step += 1
     for key in data:
         data[key] += 1
-        if data[key] > 9:
-            stack.append(key)
-            seen.add(key)
 
+    stack = [key for key in data if data[key] == 10]
     while stack:
         x, y = stack.pop()
         for dx, dy in border:
             x_ = x + dx
             y_ = y + dy
-            if 0 <= x_ < width and 0 <= y_ < height:
+            if (x_, y_) in data and data[x_, y_] < 10:
                 data[x_, y_] += 1
-                if (x_, y_) not in seen and data[x_, y_] > 9:
-                    seen.add((x_, y_))
+                if data[x_, y_] == 10:
                     stack.append((x_, y_))
 
-    flashes += len(seen)
-    for x, y in seen:
-        data[x, y] = 0
+    flashes = 0
+    for key in data:
+        if data[key] == 10:
+            data[key] = 0
+            flashes += 1
 
+    total_flashes += flashes
     if step == 100:
-        print(flashes)
+        print(total_flashes)
 
-    if len(seen) == height * width:
+    if flashes == 100:
         print(step)
         break
