64b9da598d6e5a242c723f8fe733002d343105a0
MasterMedo <mislav.vuletic@gmail.com>
2021-12-13 07:01:26 +0100

    improvement 13 2021

--- 2021/day/13.py
+++ 2021/day/13.py
@@ -1,23 +1,26 @@
 with open("../input/13.txt") as f:
-    points, folds = f.read().split("\n\n")
-    points = {tuple(map(int, line.split(","))) for line in points.split("\n")}
+    points, folds = f.read().strip().split("\n\n")
 
-    for i, line in enumerate(folds.split("\n")[:-1]):
-        coordinate, n = line.split()[-1].split("=")
-        n = int(n)
-        coordinate = 0 if coordinate == "x" else 1
-        for point in list(points):
-            if point[coordinate] > n:
-                points.remove(point)
-                point = list(point)
-                point[coordinate] = 2 * n - point[coordinate]
-                points.add(tuple(point))
+points = {tuple(map(int, line.split(","))) for line in points.split("\n")}
+for i, line in enumerate(folds.split("\n")):
+    coordinate, n = line.split()[-1].split("=")
+    n = int(n)
+    for x, y in list(points):
+        points.remove((x, y))
+        if coordinate == "x" and x > n:
+            x = 2 * n - x
 
-        if i == 0:
-            print(len(points))
+        if coordinate == "y" and y > n:
+            y = 2 * n - y
 
-for y in range(max(list(zip(*points))[1]) + 1):
-    for x in range(max(list(zip(*points))[0]) + 1):
-        print("#" if (x, y) in points else " ", end="")
+        points.add((x, y))
+
+    if i == 0:
+        print(len(points))
+
+X, Y = zip(*points)
+for y in range(max(Y) + 1):
+    for x in range(max(X) + 1):
+        print(" #"[(x, y) in points], end="")
 
     print()
