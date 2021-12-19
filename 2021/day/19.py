940c3ccda9dd4025d02948d4a8e72b70b748cc62
MasterMedo <mislav.vuletic@gmail.com>
2021-12-19 18:40:37 +0100

    improvement 19 2021

--- 2021/day/19.py
+++ 2021/day/19.py
@@ -1,4 +1,20 @@
-from itertools import starmap, cycle
+from itertools import starmap, product
+
+
+def match(i, j):
+    for negate, rotate in product(negations, rotations):
+        scanner_j = set(starmap(negate, starmap(rotate, scanners[j])))
+        for x_1, y_1, z_1 in scanners[i]:
+            for x_2, y_2, z_2 in scanner_j:
+                dx, dy, dz = x_1 - x_2, y_1 - y_2, z_1 - z_2
+                moved = set((x + dx, y + dy, z + dz) for x, y, z in scanner_j)
+
+                if len(scanners[i].intersection(moved)) >= 12:
+                    visit.append(j)
+                    unmatched.remove(j)
+                    scanners[j] = moved
+                    scanner_positions[j] = (dx, dy, dz)
+                    return
 
 
 negations = [
@@ -28,39 +44,12 @@ with open("../input/19.txt") as f:
     ]
 
 scanner_positions = [(0, 0, 0)] * len(scanners)
-found = [-1] * len(scanners)
-found[0] = 0
-scanners_found = 1
-for i in cycle(range(len(scanners))):
-    if scanners_found == len(scanners):
-        break
-
-    if found[i] == -1:
-        continue
-
-    for j in range(len(scanners)):
-        if j == i or found[j] != -1:
-            continue
-
-        for negate in negations:
-            for rotate in rotations:
-                scanner_j = set(starmap(negate, starmap(rotate, scanners[j])))
-                for x_1, y_1, z_1 in scanners[i]:
-                    for x_2, y_2, z_2 in scanner_j:
-                        if found[j] != -1:
-                            break
-
-                        dx, dy, dz = x_1 - x_2, y_1 - y_2, z_1 - z_2
-                        moved = set(
-                            (x + dx, y + dy, z + dz) for x, y, z in scanner_j
-                        )
-
-                        if len(scanners[i].intersection(moved)) >= 12:
-                            scanners[j] = moved
-                            found[j] = found[i]
-                            scanner_positions[j] = (dx, dy, dz)
-                            scanners_found += 1
-                            break
+unmatched = set(range(1, len(scanners)))
+visit = [0]
+while visit:
+    i = visit.pop()
+    for j in list(unmatched):
+        match(i, j)
 
 print(len(set.union(*scanners)))
 
