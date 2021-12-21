7c7f27af8ce560890c2144c5b92c4cfc65f0b50e
MasterMedo <mislav.vuletic@gmail.com>
2021-12-21 11:48:49 +0100

    improvement 21 2021

--- 2021/day/21.py
+++ 2021/day/21.py
@@ -5,43 +5,36 @@ with open("../input/21.txt") as f:
     player_1, player_2 = [int(line[-3:]) for line in f.readlines()]
 
 
-def deterministic_dice(player_1, player_2):
-    score_a, score_b = 0, 0
-    die = cycle(range(1, 101))
-    throws = 0
-    while True:
-        player_1 += next(die) + next(die) + next(die)
-        player_1 = (player_1 - 1) % 10 + 1
-        score_a += player_1
-        throws += 3
-        if score_a >= 1000:
-            return score_b * throws
-        player_2 += next(die) + next(die) + next(die)
-        player_2 = (player_2 - 1) % 10 + 1
-        score_b += player_2
-        throws += 3
-        if score_b >= 1000:
-            return score_a * throws
+def part_1(player_1, player_2, score_1=0, score_2=0, throws=0):
+    if score_2 >= 1000:
+        return score_1 * throws
+
+    player_1 += sum(next(deterministic_die) for _ in range(3))
+    player_1 = (player_1 - 1) % 10 + 1
+    score_1 += player_1
+    return part_1(player_2, player_1, score_2, score_1, throws + 3)
 
 
 @cache
-def quantum_dice(player_1, player_2, score_a, score_b):
-    if score_b >= 21:
+def part_2(player_1, player_2, score_1, score_2):
+    if score_2 >= 21:
         return (0, 1)
 
     wins = [0, 0]
-    for i in [1, 2, 3]:
-        for j in [1, 2, 3]:
-            for k in [1, 2, 3]:
-                new_a = (player_1 + i + j + k) % 10
-                win_b, win_a = quantum_dice(
-                    player_2, new_a, score_b, score_a + new_a + 1
+    for i in (1, 2, 3):
+        for j in (1, 2, 3):
+            for k in (1, 2, 3):
+                new_player_1 = (player_1 + i + j + k) % 10
+                new_score_1 = score_1 + new_player_1 + 1
+                win_2, win_1 = part_2(
+                    player_2, new_player_1, score_2, new_score_1
                 )
-                wins[0] += win_a
-                wins[1] += win_b
+                wins[0] += win_1
+                wins[1] += win_2
 
     return wins
 
 
-print(deterministic_dice(player_1, player_2))
-print(max(quantum_dice(player_1 - 1, player_2 - 1, 0, 0)))
+deterministic_die = cycle(range(1, 101))
+print(part_1(player_1, player_2))
+print(max(part_2(player_1 - 1, player_2 - 1, 0, 0)))
