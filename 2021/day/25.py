76bc40cbe4bdaf24c85dffc66f6a2ac21cba0358
MasterMedo <mislav.vuletic@gmail.com>
2021-12-26 16:22:03 +0100

    improvement 25 2021

--- 2021/day/25.py
+++ 2021/day/25.py
@@ -1,31 +1,27 @@
 from itertools import count
 
+
+def move(grid, char, func, moved):
+    moved_grid = grid.copy()
+    for x, y in grid:
+        x_, y_ = func(x, y)
+        if grid[x, y] == char and grid[x_, y_] == ".":
+            moved_grid[x, y] = "."
+            moved_grid[x_, y_] = char
+            moved = True
+
+    return moved_grid, moved
+
+
 with open("../input/25.txt") as f:
-    d = {(x, y): c for y, line in enumerate(f.read().strip().split("\n")) for x, c in enumerate(line)}
+    data = f.read().strip().split("\n")
 
-X = max(x for x, y in d) + 1
-Y = max(y for x, y in d) + 1
+grid = {(x, y): c for y, line in enumerate(data) for x, c in enumerate(line)}
+X = max(x for x, _ in grid) + 1
+Y = max(y for _, y in grid) + 1
 for step in count(1):
-    flag = False
-    d_ = d.copy()
-    for x, y in d:
-        x_ = (x + 1) % X
-        y_ = y
-        if d[x, y] == '>' and d[x_, y_] == '.':
-            d_[x, y] = '.'
-            d_[x_, y_] = '>'
-            flag = True
-
-    d = d_
-    d_ = d.copy()
-    for x, y in d:
-        x_ = x
-        y_ = (y + 1) % Y
-        if d[x, y] == 'v' and d[x_, y_] == '.':
-            d_[x, y] = '.'
-            d_[x_, y_] = 'v'
-            flag = True
-    d = d_
-    if not flag:
+    grid, moved = move(grid, ">", lambda x, y: ((x + 1) % X, y), False)
+    grid, moved = move(grid, "v", lambda x, y: (x, (y + 1) % Y), moved)
+    if not moved:
         print(step)
         break
