7bd9c4e56d4d6946abe984f2f0f61f0606cee4c9
MasterMedo <mislav.vuletic@gmail.com>
2022-12-03 07:14:54 +0100

    improvement 2 2022

--- 2022/day/2.py
+++ 2022/day/2.py
@@ -1,4 +1,3 @@
-data = []
 part1 = 0
 part2 = 0
 with open("../input/2.txt") as f:
@@ -6,19 +5,8 @@ with open("../input/2.txt") as f:
         a, b = line.split()
         a = int(a.translate(str.maketrans("ABC", "123")))
         b = int(b.translate(str.maketrans("XYZ", "123")))
-        part1 += b
-        if a == b:  # draw
-            part1 += 3
-        elif a == (b - 2) % 3 + 1:  # win
-            part1 += 6
-        if b == 1:  # lose
-            part2 += (a - 2) % 3 + 1
-        elif b == 2:  # draw
-            part2 += a
-            part2 += 3
-        else:  # win
-            part2 += a % 3 + 1
-            part2 += 6
+        part1 += b + ((b - a + 1) % 3) * 3
+        part2 += (a + b) % 3 + 1 + (b - 1) * 3
 
 print(part1)
 print(part2)
